8c1b998afb59ec917801f9226dc42ddf131f39d4
D4-Developer <dev.dr009@gmail.com>
2021-02-02 14:24:41 +0530

    fundamentals done

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,44 @@
+// #0
+/*
+let country = 'india';
+let continent = 'asia';
+let population = 12100
+
+console.log(country);
+console.log(continent)
+console.log(population) 
+*/
+
+// #1
+/*const markMass = 95, johnMass = 85;
+const markHeight = 1.88, johnHeight = 1.76;
+
+const markBMI = markMass / (markHeight * markHeight);
+const johnBMI = johnMass / (johnHeight * johnHeight);
+
+const markHigherBMI = markBMI > johnBMI;
+
+console.log(markBMI, johnBMI, markHigherBMI)
+*/
+
+// #3
+// const dolphine1 = 96, dolphine2 = 108, dolphine3 = 89
+// const koalas1 = 88, koalas2 = 91, koalas3 = 110
+
+// const avgDolphine = (dolphine1 + dolphine2 + dolphine3) / 3
+// const avgKoalas = (koalas1 + koalas2 + koalas3) / 3
+
+// if (avgDolphine === avgKoalas)
+//     console.log("Draw")
+// else if (avgDolphine > avgKoalas)
+//     console.log("Dolphine team wins")
+// else
+//     console.log("Koalas team wins")
+
+
+// #4
+// const bill = 40
+// const tip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .20
+
+// const str = `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+// console.log(str)
---
+++ 01-Fundamentals-Part-1/starter/index.js
@@ -0,0 +1,66 @@
+// let js = 'amazing';
+// js = 'not at all';
+// if (js === 'amazing') alert("Javascript is amazing");
+// else alert("is not...");
+
+// console.log(22 + 10 + 5 + 5);
+
+// const firstName = 'darshan'
+// const job = 'flutter developer'
+// const birthYear = 2000
+// const currYear = 2021
+// const darshan = "I'm " + firstName + ', a '+ (currYear - birthYear) + ' years old ' + job + '!';
+
+// // ES6 features
+// const darshanNew = `I'm ${firstName}, a ${currYear - birthYear} years old ${job}`
+// console.log(darshanNew)
+
+// console.log(`String
+// with multiple
+// lines`);
+
+// console.log('23' + '2')
+// console.log('23' - '2')
+// console.log('23' * '2')
+// console.log('23' - '2')
+// console.log('23' > '22')
+
+// let n = '1' + 1
+// n = n - 1
+// console.log(n)
+
+// let h;
+// console.log(Boolean(h))
+// console.log(Boolean(0))
+// console.log(Boolean(1))
+// console.log(Boolean(undefined))
+// console.log(Boolean('darshan'))
+// console.log(Boolean({}))
+// console.log(Boolean(''))
+
+// const favourite = prompt("what's your favourite number?")
+// console.log(favourite)
+// console.log(typeof favourite)
+// if (favourite == 23)
+// console.log(" equal 23 number")
+// else 
+// console.log(" NOT EQUAL")
+// if (favourite === 23)
+// console.log(" equal 23 number")
+// else 
+// console.log(" NOT EQUAL")
+
+// const day = 'monday'
+// switch(day) {
+//     case 'monday': //  day === 'monday
+
+//     break;
+// }
+// const num = 11
+// switch(num) {
+//     case '11':
+//         console.log("oh yes 11")
+//     break
+//     default:
+//         console.log("default")
+// }
