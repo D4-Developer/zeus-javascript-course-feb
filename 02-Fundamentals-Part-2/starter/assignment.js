f65ff714a20f3fe012df95ec7ee7684505f46755
D4-Developer <dev.dr009@gmail.com>
2021-02-04 18:55:31 +0530

    assignment #3

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -30,3 +30,28 @@
 // });
 // console.log(tips)
 // console.log(totals)
+
+// #3
+// const mark = {
+//     fullName: 'mark',
+//     mass: 78,
+//     height: 1.69,
+//     calcBMI: function() {
+//         this.bmi = this.mass / (this.height ** 2);
+//         return this.bmi;
+//     }
+// };
+// const john = {
+//     fullName: 'john',
+//     mass: 92,
+//     height: 1.95,
+//     calcBMI: function() {
+//         this.bmi = this.mass / (this.height ** 2);
+//         return this.bmi;
+//     }
+// };
+// const result = mark.calcBMI() > john.calcBMI() 
+//     ? `mark's BMI (${mark.bmi} is higher than john's (${john.bmi}))` 
+//     : `john's BMI (${john.bmi} is higher than mark's (${mark.bmi}))`;
+
+// console.log(result);
