d2ce08a8ce4ffa4c10dfdba1c04e545954002ece
D4-Developer <dev.dr009@gmail.com>
2021-02-04 17:50:11 +0530

    assignment #1

---
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -0,0 +1,16 @@
+'use strict';
+// #1
+// const calcAverage = (v1, v2, v3) => (v1 + v2 + v3) / 3;
+
+// function checkWinner(avgDolphins, avgKoalas) {
+//     console.log(avgDolphins, avgKoalas);
+//     if (avgDolphins >= 2 * avgKoalas)
+//         console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+//     else if (avgKoalas >= 2 * avgDolphins)
+//         console.log(`Dolphins win (${avgDolphins} vs. ${avgKoalas})`);
+// }
+
+// const avgDolphins = calcAverage(85, 54, 41);
+// const avgKoalas = calcAverage(23, 34, 27);
+
+// checkWinner(avgDolphins, avgKoalas);
