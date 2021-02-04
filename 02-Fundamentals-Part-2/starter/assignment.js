54d49e8f51401ed19a689fa309284b53c3d6820d
D4-Developer <dev.dr009@gmail.com>
2021-02-04 18:05:31 +0530

    assignment #2

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -14,3 +14,19 @@
 // const avgKoalas = calcAverage(23, 34, 27);
 
 // checkWinner(avgDolphins, avgKoalas);
+
+// #2
+// function calcTip(bill) {
+//         return bill >= 50 && bill <= 300 
+//             ? bill * 0.15 : bill * 0.20;
+// }
+// const bills = [100, 125, 555, 44];
+// const tips = [];
+// const totals = [];
+// bills.forEach(bill => {
+//     const tip = calcTip(bill);
+//     tips.push(tip);
+//     totals.push(tip + bill);
+// });
+// console.log(tips)
+// console.log(totals)
