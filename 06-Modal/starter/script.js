3e5be3f9089ad111f7609783ab01ef01637791e8
D4-Developer <dev.dr009@gmail.com>
2021-02-10 11:45:27 +0530

    Added overlay onClick of modals elements

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,8 +3,13 @@
 // document.querySelector() only returns the first matched element only...
 // document.querySelectorAll()
 const modals = document.querySelectorAll('.show-modal');
+const overlay = document.querySelector('.overlay');
+const closeModal = document.querySelector('.close-modal');
+
 console.log(modals);
 console.log(typeof modals );
 
 for (let i = 0; i < modals.length; ++i)
-    console.log(modals[i].textContent);
+    console.log(modals[i].addEventListener('click', function() {
+        overlay.classList.remove('hidden');
+    }));
