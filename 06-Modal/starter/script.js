005e2a10457d219369bc306d2bf7936f891b3a72
D4-Developer <dev.dr009@gmail.com>
2021-02-10 11:37:45 +0530

    querySelectorAll(): for loop to iterate over arrayObject

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -3,4 +3,7 @@
 // document.querySelectorAll
 const modals = document.querySelectorAll('.show-modal');
 console.log(modals);
+console.log(typeof modals );
 
+for (let i = 0; i < modals.length; ++i)
+    console.log(modals[i].textContent);
