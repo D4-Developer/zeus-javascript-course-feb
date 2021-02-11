73318b91d1039904ab20864af07072dcaf830852
D4-Developer <dev.dr009@gmail.com>
2021-02-11 16:07:45 +0530

    global & function scope simple ex....

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2021 - birthYear; // function--scope
+    console.log(firstName); // np
+    return age;
+}
+
+const firstName = 'darshan'; // global--scope
+calcAge(2000);
