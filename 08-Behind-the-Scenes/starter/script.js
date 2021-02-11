2456eb5b2044081b85dffd5768044a36c2ac7f63
D4-Developer <dev.dr009@gmail.com>
2021-02-11 16:13:05 +0530

    Revert "global but ReferenceError: can't access lexical declaration of var before initialization"
    This reverts commit 2b29dceb3b6bc4c13e8cc5b4f3b6a3d5714b16b6.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -6,9 +6,5 @@ function calcAge(birthYear) {
     return age;
 }
 
-calcAge(2000);
 const firstName = 'darshan'; // global--scope
-
-// lexical error :) ReferenceError: can't access lexical declaration 'firstName' before initialization....
-//// calcAge(2000); // this will generate the error:) 
-//// const firstName = 'darshan';
+calcAge(2000);
