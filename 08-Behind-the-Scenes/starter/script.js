c754fd364934b2963009ec4f9d5040d853c9b980
D4-Developer <dev.dr009@gmail.com>
2021-02-11 16:13:33 +0530

    global but ReferenceError: can't access lexical declaration of var before initialization

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -8,3 +8,7 @@ function calcAge(birthYear) {
 
 const firstName = 'darshan'; // global--scope
 calcAge(2000);
+
+// lexical error :) ReferenceError: can't access lexical declaration 'firstName' before initialization....
+//// calcAge(2000); // this will generate the error:) 
+//// const firstName = 'darshan';
