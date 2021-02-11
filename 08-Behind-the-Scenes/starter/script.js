3d91238ac028708e4cbd0ded2a0426ac3c5b418c
D4-Developer <dev.dr009@gmail.com>
2021-02-11 16:31:53 +0530

    block scope ex. & error if accessed outside block

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -7,7 +7,13 @@ function calcAge(birthYear) {
     function printAge() {
         const output = `${firstName}, you are ${age}, born in ${birthYear}`; // np inherites the parent's scope
         console.log(output);
+        if (birthYear >= 1996 && birthYear <= 2006) {
+            const ot = `oh you are millenial, ${firstName}`;
+            console.log(ot);
+        }
+        console.log(ot); // -- error --
     }
+
 
     printAge();
     return age;
@@ -17,5 +23,5 @@ const firstName = 'darshan'; // global--scope
 calcAge(2000);
 
 // lexical error :) ReferenceError: can't access lexical declaration 'firstName' before initialization....
-//// calcAge(2000); // this will generate the error:) 
+//// calcAge(2000); // this will generate the -- error -- 
 //// const firstName = 'darshan';
