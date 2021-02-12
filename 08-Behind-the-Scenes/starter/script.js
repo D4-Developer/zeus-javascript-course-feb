4246cef2fa6e8e1a241b1baf3f5133486fedc3a4
D4-Developer <dev.dr009@gmail.com>
2021-02-12 18:01:19 +0530

    #argument-keyword :) fnDeclaration/fnExpression/fnArraow => T-T-F

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -117,35 +117,50 @@
 // f(); // -- error -- because this keyword is undefined & this.birthYear throws the error
 
 ///////////////////////////////////////
-// Regular Functions vs. Arrow Functions
-// var firstName = 'Matilda';
+// // Regular Functions vs. Arrow Functions
+// // var firstName = 'Matilda';
 
-const jonas = {
-    firstName: 'Jonas',
-    year: 1991,
-    calcAge: function () {
-      // console.log(this);
-      console.log(2037 - this.year);
+// const jonas = {
+//     firstName: 'Jonas',
+//     year: 1991,
+//     calcAge: function () {
+//       // console.log(this);
+//       console.log(2037 - this.year);
   
-      // Solution 1
-      // const self = this; // self or that
-      // const isMillenial = function () {
-      //   console.log(self);
-      //   console.log(self.year >= 1981 && self.year <= 1996);
-      // };
+//       // Solution 1
+//       // const self = this; // self or that
+//       // const isMillenial = function () {
+//       //   console.log(self);
+//       //   console.log(self.year >= 1981 && self.year <= 1996);
+//       // };
   
-      // Solution 2
-      const isMillenial = () => {
-        console.log(this);
-        console.log(this.year >= 1981 && this.year <= 1996);
-      };
-      isMillenial();
-    },
+//       // Solution 2
+//       const isMillenial = () => {
+//         console.log(this);
+//         console.log(this.year >= 1981 && this.year <= 1996);
+//       };
+//       isMillenial();
+//     },
   
-    greet: () => {
-      console.log(this);
-      console.log(`Hey ${this.firstName}`);
-    },
+//     greet: () => {
+//       console.log(this);
+//       console.log(`Hey ${this.firstName}`);
+//     },
+//   };
+//   jonas.greet();
+//   jonas.calcAge();
+
+// arguments keyword
+const addExpr = function (a, b) {
+    console.log(arguments);
+    return a + b;
   };
-  jonas.greet();
-  jonas.calcAge();
+  addExpr(2, 5);
+  addExpr(2, 5, 8, 12);
+  
+  var addArrow = (a, b) => {
+    console.log(arguments);
+    return a + b;
+  };
+  addArrow(2, 5, 8);
+  
