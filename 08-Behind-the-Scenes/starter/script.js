0e1c5b3910017ea98d923ebe0dc827f536ed86bf
D4-Developer <dev.dr009@gmail.com>
2021-02-12 17:42:26 +0530

    # this keyword:) 1] a window object 2]fnDeclaration,fnExpression,fnArrow \n 3] this keyword reference to on which object the function() is invoked.... ex) darshan.calAge() => this is point to darshan object if calcAge() use this keyword....

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,4 +1,4 @@
-// 'use strict';
+'use strict';
 
 // function calcAge(birthYear) {
 //     const age = 2021 - birthYear; // function--scope
@@ -71,11 +71,47 @@
 
 // window object of current page....
 
-var x = 1; // var are defined in creation phase
-let y = 1; // const & let are actually created in execution phase
-const z = 1;
+// var x = 1; // var are defined in creation phase
+// let y = 1; // const & let are actually created in execution phase
+// const z = 1;
 
-// console.log(window);
-console.log(x === window.x); // true
-console.log(y === window.y); // false
-console.log(z === window.z); // false
+// // console.log(window);
+// console.log(x === window.x); // true
+// console.log(y === window.y); // false
+// console.log(z === window.z); // false
+
+// # this keyword.... not a STATIC value
+// special variable that is created for every context (every function)
+// this ketyword never point to a Function or a variable
+
+// console.log(this); // -- window object --
+
+function cAge(birthYear) {
+    // console.log(this); // -- window object --
+    console.log(2021 - birthYear);
+};  cAge(2000);
+const cAgeEx = function (birthYear) {
+    console.log(this); // -- undefined (strict-mode) -- otherwise window object
+
+};  cAgeEx(2000);
+const cAgeArr = birthYear => {
+    console.log(this); // arrow fn don't have this scope 
+    // => so it will use it's parent's this keyword.. in this it's window object
+};  cAgeArr(2020);
+
+const darshan = {
+    birthYear: 2000,
+    calcAge: function() {
+        console.log(2020 - this.birthYear);
+    }
+};
+darshan.calcAge();
+
+const tirth = {
+    birthYear: 1999
+};
+tirth.calcAge = darshan.calcAge; // method borrowing....
+tirth.calcAge();
+
+const f = darshan.calcAge;
+f(); // -- error -- because this keyword is undefined & this.birthYear throws the error
