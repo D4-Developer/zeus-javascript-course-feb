a3e94a8a669a781330f4219c7ee4eb363fbecd24
D4-Developer <dev.dr009@gmail.com>
2021-02-15 19:33:53 +0530

    #done coding challage #1

---
+++ 09-Data-Structures-Operators/starter/assignment.js
@@ -0,0 +1,73 @@
+'use-strict'
+// // ::::: #1
+
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+        'Neuer',
+        'Pavard',
+        'Martinez',
+        'Alaba',
+        'Davies',
+        'Kimmich',
+        'Goretzka',
+        'Coman',
+        'Muller',
+        'Gnarby',
+        'Lewandowski',
+        ],
+        [
+        'Burki',
+        'Schulz',
+        'Hummels',
+        'Akanji',
+        'Hakimi',
+        'Weigl',
+        'Witsel',
+        'Hazard',
+        'Brandt',
+        'Sancho',
+        'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: [
+        'Lewandowski', 'Gnarby', 'Lewandowski','Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+    }
+};
+
+const [players1, players2] = game.players;
+console.log(players1);
+console.log(players2);
+
+const [gk1, ...filedPlayers1] = players1;
+console.log(gk1);
+const [gk2, ...filedPlayers2] = players2;
+console.log(gk2);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers.length);
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+const {team1 , x:draw, team2} = game.odds;
+console.log(team1, draw, team2);
+
+function printGoals(...receivedPlayers) {
+    console.log(receivedPlayers);
+    console.log(`${receivedPlayers.length} goals were scored`);    
+}
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
