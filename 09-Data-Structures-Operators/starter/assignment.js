a26e794a402fbfc905f1f4aec2cffc115956f177
D4-Developer <dev.dr009@gmail.com>
2021-02-16 14:00:52 +0530

    done: coding-challage #2

--- 09-Data-Structures-Operators/starter/assignment.js
+++ 09-Data-Structures-Operators/starter/assignment.js
@@ -43,31 +43,60 @@ const game = {
     }
 };
 
-const [players1, players2] = game.players;
-console.log(players1);
-console.log(players2);
+// const [players1, players2] = game.players;
+// console.log(players1);
+// console.log(players2);
 
-const [gk1, ...filedPlayers1] = players1;
-console.log(gk1);
-const [gk2, ...filedPlayers2] = players2;
-console.log(gk2);
+// const [gk1, ...filedPlayers1] = players1;
+// console.log(gk1);
+// const [gk2, ...filedPlayers2] = players2;
+// console.log(gk2);
 
-const allPlayers = [...players1, ...players2];
-console.log(allPlayers.length);
+// const allPlayers = [...players1, ...players2];
+// console.log(allPlayers.length);
 
-const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
-console.log(players1Final);
+// const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+// console.log(players1Final);
 
-const {team1 , x:draw, team2} = game.odds;
-console.log(team1, draw, team2);
+// const {team1 , x:draw, team2} = game.odds;
+// console.log(team1, draw, team2);
 
-function printGoals(...receivedPlayers) {
-    console.log(receivedPlayers);
-    console.log(`${receivedPlayers.length} goals were scored`);    
+// function printGoals(...receivedPlayers) {
+//     console.log(receivedPlayers);
+//     console.log(`${receivedPlayers.length} goals were scored`);    
+// }
+
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals(...game.scored);
+
+// team1 < team2 && console.log('Team 1 is more likely to win');
+// team1 > team2 && console.log('Team 2 is more likely to win');
+
+
+// //// :::: #2
+
+// 1)
+for (const [index, playerName] of game.scored.entries()) 
+    console.log(`Goal ${1 + index}: ${playerName}`);
+
+// 2) 
+let totalOdd = 0;
+const odds = Object.values(game.odds);
+for (const odd of odds)
+    totalOdd += odd;
+
+console.log(totalOdd /= odds.length);
+
+// 3)
+for (const [teamName ,oddValue] of Object.entries(game.odds)) {
+    const teamStr = game[teamName] ? `Victory ${game[teamName]}` : 'Draw';
+    console.log(`Odd of ${teamStr}: ${oddValue}`);
 }
 
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-printGoals(...game.scored);
+// 4)
+const scorers = {};
 
-team1 < team2 && console.log('Team 1 is more likely to win');
-team1 > team2 && console.log('Team 2 is more likely to win');
+for (player of game.scored)
+    scorers[player] = scorers[player] + 1 ||  1;
+
+console.log(scorers);
