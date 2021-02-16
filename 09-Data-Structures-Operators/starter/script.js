e675278c08ca8fb6a5a2a856dbc1c12efa4efb06
D4-Developer <dev.dr009@gmail.com>
2021-02-16 12:10:32 +0530

    #optional-chaining :: object.fucntionName?.() ?? 'else method execution or log in console about method not exist' => calling this method, if functionName not exist , returns undefined instead of error.... here right side of nullish operator will be executed... ?? ____

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -336,3 +336,7 @@ for (const day of days) {
   const open = restaurant.openingHours[day]?.open ?? 'closed';
   console.log(`On, ${day}, we open at ${open}`);
 }
+
+// Methods
+console.log(restaurant.order?.(0,1) ?? 'Method does not exist');
+console.log(restaurant.reserve?.(0,1) ?? 'Method does not exist');
