ceb2a502201159bbce51b4b931b2ceff222a1f71
D4-Developer <dev.dr009@gmail.com>
2021-02-18 16:52:01 +0530

    note: first-class function & higher-order function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -26,6 +26,8 @@ createBooking('LH123', 4, 1200);
 */
 // object vs. primitives data type in function args ,
 // changing value of that may be lead to a completely different situation
+/*
+
 const flight = 'LH123';
 const darshan = {
     name: 'darshan rathod',
@@ -57,4 +59,37 @@ function newPassport(person) {
 newPassport(darshan);
 checkIn(flight, darshan); // wrong passport as 
 // object.passport is changed inside newPassport function
+
+*/
+
+// first-class functions
+function oneWord(str) {
+    // return the string without any spaces
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+function upperFirstWord(str) {
+    const [firstWord, ...otherWords] = str.split(' ');
+    return [firstWord.toUpperCase(), ...otherWords].join(' ');
+}
+
+// higher-order function
+function transformer (str, fn) {
+    console.log(`Orifginal string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+    console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('Javascript is fun!', upperFirstWord);
+transformer('Flutter is love!!!', oneWord);
+
+
+// callback functions
+// allows to do abstraction
+
+function high5() {
+    console.log('hiiii');
+}
+
+document.body.addEventListener('click', high5);
 
