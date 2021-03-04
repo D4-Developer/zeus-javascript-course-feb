006cff219f39da8e8b8c225b48d323773711af2e
D4-Developer <dev.dr009@gmail.com>
2021-03-04 16:10:34 +0530

    feat: OOP with function Constructor

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+    console.log(this);
+
+    // Instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // Never do this for class methods
+    this.calcAge = function () {
+        console.log(2037 - this.birthYear);
+    }
+}
+
+// use new keyword to make a instance of class
+const jonas = new Person('Jonas', 1991);
+console.log(jonas);
+
+// 1. new {} is created
+// 2. function is called, this = {}
+// 3. {} is linked to prototype
+// 4. function authmatically return {}
+
+const darshan = new Person('Darshan', 2000);
+const tirth = new Person('Tirth', 1999);
+console.log(darshan, tirth);
+
+const a = 'ABC';
+// return True / False
+console.log(a instanceof Person); // False
