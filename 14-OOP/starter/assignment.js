bf2297696275ff1a63903cf0c0acab9131fc9ba7
D4-Developer <dev.dr009@gmail.com>
2021-03-04 20:04:27 +0530

    done: coding challange #2

--- 14-OOP/starter/assignment.js
+++ 14-OOP/starter/assignment.js
@@ -1,5 +1,6 @@
 'use strict'
 
+// #1
 const Car = function(make, speed) {
     this.make = make;
     this.speed = speed;
@@ -22,3 +23,39 @@ BMW.accelerate();
 BMW.accelerate();
 BMW.brake();
 BMW.accelerate();
+
+
+
+// #2
+class CarCl {
+    constructor(make, speed) {
+      this.make = make;
+      this.speed = speed;
+    }
+  
+    accelerate() {
+      this.speed += 10;
+      console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+  
+    brake() {
+      this.speed -= 5;
+      console.log(`${this.make} is going at ${this.speed} km/h`);
+    }
+  
+    get speedUS() {
+      return this.speed / 1.6;
+    }
+  
+    set speedUS(speed) {
+      this.speed = speed * 1.6;
+    }
+}
+  
+const ford = new CarCl('Ford', 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford);
